b7439117ee414f18386d6135410c425c02d6043a
ebeshero <ebbondar@gmail.com>
2024-11-06 01:20:33 -0500

    Updates

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
+// import * as THREE from 'three';
 import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
-// import './style.css'
+// import './style.css';
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.js'
 
+
 // Setup
 const scene = new THREE.Scene();
-const libraryTexture = new THREE.TextureLoader().load('images/library.jpg')
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // create a new renderer by instating the canvas element in our HTML // file
@@ -18,39 +18,62 @@ const renderer = new THREE.WebGLRenderer({
 });
 const geometry = new THREE.BoxGeometry(10, 10, 10);
 
+// Textures
+const libraryTexture = new THREE.TextureLoader().load('images/library.jpg');
+const wowTexture = new THREE.TextureLoader().load('images/warofworldswordcloud.png');
+const msTexture = new THREE.TextureLoader().load('images/msImage.png');
+const smaugTexture = new THREE.TextureLoader().load('images/tolkien-smaug-artwork-2.jpg');
+
 //set the color of the basic material in the object parameters `{}`
 
-const material = new THREE.MeshStandardMaterial( { color:0xFF6347} );
+const material = new THREE.MeshStandardMaterial( { color:0x6347FF, map:smaugTexture} );
 
 const cube = new THREE.Mesh( geometry, material );
-cube.position.z = -15;
-cube.position.x = -15;
+cube.position.z = -25;
+cube.position.x = 8;
 cube.rotation.x = 2;
-cube.rotation.y = .5;
+cube.rotation.y = .8;
 
 
-const ico = new THREE.IcosahedronGeometry(10);
-const icoMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+const ico = new THREE.IcosahedronGeometry(15, 1);
+const icoMaterial = new THREE.MeshPhongMaterial({ color:0x33AADD, map:msTexture, shininess:75});
+
 const icoMesh = new THREE.Mesh(ico, icoMaterial);
+// object.position.set ( x, y, z );
+icoMesh.position.set(30, 10, -20)
+// icoMesh.position.z= -20;
+// icoMesh.position.x= 30;
+
+const sphereGeo = new THREE.SphereGeometry( 20, 32, 16 );
+const sphereMaterial = new THREE.MeshBasicMaterial( { map:wowTexture } );
+const sphere = new THREE.Mesh( sphereGeo, sphereMaterial ); scene.add( sphere );
+// object.position.set ( x, y, z );
+sphere.position.set(-30,5,-45)
+
 
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, -10, 10);
+pointLight.position.set(-1, -10, -10);
+
+const pointLight2 = new THREE.PointLight(0xffffff);
+pointLight2.position.set(50, 30, -20);
+
+const pointLight3 = new THREE.PointLight(0xffffff);
+pointLight3.position.set(200, -100, 50);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 ambientLight.position.set(25, -15, -400);
 
+
 scene.add(pointLight);
-scene.add(ambientLight);
-
-scene.add(icoMesh);
-
-icoMesh.position.z= -15;
-icoMesh.position.x= 15;
-
+scene.add(pointLight2);
+scene.add(pointLight3);
 scene.background = libraryTexture;
-scene.add( cube );
+scene.add(cube);
+scene.add(ambientLight);
+scene.add(icoMesh);
+scene.add(sphere)
 
 
 //Render the scene:
@@ -58,6 +81,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(50);
 camera.position.setX(-3);
+camera.position.setY(10);
 renderer.render(scene, camera);
 
 function animate() {
@@ -66,8 +90,10 @@ function animate() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     // rotate the icosahedron a little faster in the opposite direction:
-    icoMesh.rotation.z += -0.03
-    icoMesh.rotation.y += -0.03
+    icoMesh.rotation.x += -0.001
+    icoMesh.rotation.z += -0.001
+    sphere.rotation.x += -0.001
+    sphere.rotation.z += -0.001
 
     renderer.render( scene, camera );
 }
